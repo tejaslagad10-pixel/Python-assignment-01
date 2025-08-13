213fc3988a33855bc5e8911f829fe8e07ecb29bd
tejaslagad10-pixel <tejaslagad10@gmail.com>
2025-08-13 18:22:31 +0530

    Add files via upload

---
+++ Assignment 01.py
@@ -0,0 +1,15 @@
+#Assigning numbers
+num1 =30
+num2 =10
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+print("First number: ", num1)
+print("Second number: ", num2)
+print("\nAddition: ", addition)
+print("subtraction: ", subtraction)
+print("multiplication: ", multiplication)
+print("Division: ", division)
+
---
+++ Assignment01 Q2.py
@@ -0,0 +1,11 @@
+# Task 2: Create a Personalized Greeting
+
+# Store your first and last name
+first_name = "Tejesh"
+last_name = "Lagad"
+
+# Concatenate to make full name
+full_name = first_name + " " + last_name
+
+# Print personalized greeting
+print(f"Hello, {full_name}! Welcome to the Python program.")
